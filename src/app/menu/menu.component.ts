9e051fcfbd306f954bd467c4df8159f0139d0d5a
Vinicio <viniciojoj@gmail.com>
2020-07-21 22:54:19 -0600

    Components 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
-
-	dishes: Dish[] = [
+ const DISHES: Dish[] = [
 	   {
 	     id: '0',
 	     name: 'Uthappizza',
@@ -52,6 +45,15 @@ export class MenuComponent implements OnInit {
 	   }
 	  ];
 
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+	dishes: Dish[] = DISHES;
+	selectedDish = DISHES[0];
   constructor() { }
 
   ngOnInit(): void {
