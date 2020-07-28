0dfed84d689892a45fbe843b0f34372c1a70c33a
Vinicio <viniciojoj@gmail.com>
2020-07-27 20:54:13 -0600

    Single Page Applications Part 1

--- src/app/service/dish.service.ts
+++ src/app/service/dish.service.ts
@@ -4,13 +4,21 @@ import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DishService {
 
-  constructor() { }
+	constructor() { }
 
-  getDishes(): Dish[] {
-    return DISHES;
-  }
+	getDishes(): Dish[] {
+		return DISHES;
+	}
+
+	getDish(id: string): Dish {
+		return DISHES.filter((dish) => (dish.id === id))[0];
+	}
+
+	getFeaturedDish(): Dish {
+		return DISHES.filter((dish) => dish.featured)[0];
+	}
 }
