e4fa02cc9c03efa2b9dc2fd3631d17db8fa50118
Vinicio <viniciojoj@gmail.com>
2020-08-06 13:30:54 -0600

    Reactive Forms part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,9 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms'; 
 
 import { AppComponent } from './app.component';
@@ -50,13 +53,16 @@ import { LoginComponent } from './login/login.component';
 		MatListModule,
 		MatGridListModule,
 		MatCardModule,
-		MatButtonModule,
+		MatButtonModule,		
 		AppRoutingModule,
 		MatDialogModule,
 		MatFormFieldModule, 
 		MatInputModule,
 		MatCheckboxModule,
-		FormsModule
+		MatSelectModule,
+   	MatSlideToggleModule,
+		FormsModule,
+		ReactiveFormsModule
 	],
 	providers: [
 		DishService,
